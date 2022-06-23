ff3348396861bb0ba6b1da4c1c2cbb92cd9ff011
blendthink <info@blendthink.dev>
2022-06-23 20:36:46 +0900

    add example

---
+++ example/lib/gen/pubspec.dart
@@ -0,0 +1,19 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+import 'package:pubspec_parse/pubspec_parse.dart';
+
+final pubspec = Pubspec.parse("""name: example
+description: A example of pubspec_builder.
+version: 1.0.0
+
+environment:
+  sdk: '>=2.17.3 <3.0.0'
+
+dependencies:
+
+dev_dependencies:
+  pubspec_builder:
+    path: ..
+  build_runner: ^2.1.11
+""");
---
+++ example/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:example/gen/pubspec.dart';
+
+void main() {
+  print(pubspec.name);
+}
